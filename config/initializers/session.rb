b7c9256bd47291a65ee79c3718bbe2d5b43099e8
gitnyasha <marshall.chikari@gmail.com>
2021-09-19 19:06:35 +0200

    created user sessions and registrations

--- config/initializers/session.rb
+++ config/initializers/session.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_hekani_social_media", domain: "hekani-social-media.herokuapp.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_hekani_social_media", domain: "hekani-social-media.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_hekani_social_media"
+end
