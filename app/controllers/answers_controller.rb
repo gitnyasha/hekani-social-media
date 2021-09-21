a056d37168681880b15b03c11ca5f005a686b04c
gitnyasha <marshall.chikari@gmail.com>
2021-09-21 15:51:41 +0200

    created controllers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,49 @@
 class AnswersController < ApplicationController
+  def index
+    @answers = Answer.all
+    render json: @answers
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+    @comments = Comment.where(answer_id: @answer.id)
+    render json: { answer: @answer, comments: @comments }
+  end
+
+  def create
+    current_user = User.find(session[:user_id])
+    @answer = current_user.answers.create!(answer_params)
+    if @answer.save
+      render json: { status: "success" }
+    else
+      render json: { error: "Could not save answer" }
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+    if @answer.update(answer_params)
+      render json: { status: "success" }
+    else
+      render json: { status: "error update the post" }
+    end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    if @answer.destroy
+      render json: { status: "success" }
+    else
+      render json: { status: "error destroy the post" }
+    end
+  end
+
+  private
+
+  def answers_params
+    params.require(:question).permit(:title)
+  end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,51 @@
 class QuestionsController < ApplicationController
+  include CurrentUserConcern
+
   def index
     @questions = Question.all
     render json: @questions
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    @answers = Answer.where(question_id: @question.id)
+    render json: { question: @question, answers: @answers }
+  end
+
+  def create
+    current_user = User.find(session[:user_id])
+    @question = current_user.questions.create!(questions_params)
+    if @question.save
+      render json: { status: "success" }
+    else
+      render json: { status: "error create the post" }
+    end
+  end
+
+  def edit
+  end
+
+  # edit the question
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(questions_params)
+      render json: { status: "success" }
+    else
+      render json: { status: "error update the post" }
+    end
+  end
+
+  def destroy
+    if @question.destroy
+      render json: { status: "success" }
+    else
+      render json: { status: "error destroy the post" }
+    end
+  end
+
+  private
+
+  def questions_params
+    params.require(:question).permit(:title)
   end
 end
