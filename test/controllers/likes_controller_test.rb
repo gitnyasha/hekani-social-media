ed873d2ff226bc888bd7da39bceaf685fb50ba8f
gitnyasha <marshall.chikari@gmail.com>
2021-09-22 13:23:01 +0200

    add tests

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
+  test "should create like" do
+    assert_difference("Like.count") do
+      like likes_url, params: { like: { user_id: 1, article_id: 1 } }
+    end
+    assert true
+  end
+
+  # test "should destroy like" do
+  #   assert_difference("Like.count", -1) do
+  #     delete like_url(@like)
+  #   end
+
   #   assert true
   # end
 end
