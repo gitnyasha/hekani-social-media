e0883e03b2b465720803bf476366bb3f8e95a86b
gitnyasha <marshall.chikari@gmail.com>
2022-05-19 18:21:58 +0200

    add message

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -4,14 +4,14 @@ module Api
       def create
         user = User.find(params[:followed_id])
         if current_user.follow(user)
-          render json: { status: "Successfully followed" }
+          render json: { status: "Successfully followed", message: "You are now following #{user.name}" }
         end
       end
 
       def destroy
         user = Relationship.find(params[:id]).followed
         if current_user.unfollow(user)
-          render json: { status: "Successfully unfollowed" }
+          render json: { status: "Successfully unfollowed", message: "You are no longer following #{user.name}" }
         end
       end
     end
