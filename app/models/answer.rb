6fa35749243450d517b9341fe8d84f4621e498bc
gitnyasha <marshall.chikari@gmail.com>
2022-05-19 17:00:03 +0200

    fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,7 @@ class Answer < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
-  # has_rich_text :title
+  has_rich_text :title
 
   def voted_by?(user)
     votes.exists?(user_id: user.id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
   has_many :questions
   has_many :answers, dependent: :destroy
   has_many :comments, dependent: :destroy
